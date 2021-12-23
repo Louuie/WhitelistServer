3f25a885f853e59156d6a745569f275d9b4f10a8
eliasdandouch <elias_dandouch1@my.vcccd.edu>
2021-12-23 02:53:10 -0800

    added favicon to express

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express()
 const pug = require('pug')
+const favicon = require('serve-favicon')
+const path = require('path')
 const userRouter = require('./routes/users')
 const whitelistRouter = require('./routes/whitelist')
 global.dotenv = require('dotenv')
 
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 app.use(express.static('public'))
 app.use(express.static('images'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-
 app.set('view engine', 'pug')
-
 
 app.use('/users', userRouter)
 app.use('/whitelist', whitelistRouter)
